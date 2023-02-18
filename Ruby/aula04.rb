aee5b0a88452f53a3c9de4ec24adf2de93b25443
José Augusto <103865017+Gutobjj@users.noreply.github.com>
2023-02-18 19:36:16 -0300

    commit 01

---
+++ Ruby/aula04.rb
@@ -0,0 +1,16 @@
+#ENTRADA DE DADOS
+
+# NOME 
+print "Digite seu nome: "
+nome = gets.chomp
+
+#sobrenome
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+# idade + gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+
+#SAIDA DE DADOS
+puts"Oi! #{nome} #{sobrenome} , Seja bem vindo! sua idade é #{idade}."
---
+++ Ruby/soma05.rb
@@ -0,0 +1,13 @@
+#number 01
+print "Insira um numero inteiro: "
+num1 = gets.chomp.to_i
+
+#number 02
+print "Insira outro numero inteiro: "
+num2 = gets.chomp.to_i
+
+#soma
+soma = num1 * num2
+
+#resultado
+puts"A soma dos seus numeros deu #{soma}!"
