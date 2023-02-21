ddda38b8e3f58c3ef9a6499621856de971050d20
José Augusto <gutuhsanttos29@gmail.com>
2023-02-21 14:01:20 -0300

    002

--- soma06.rb
+++ soma06.rb
@@ -1,5 +1,19 @@
-puts "Insira um numero: "
-number = [gets.chomp.to_i]
-number.push{} 
+# criar um array vazio
+numeros = []
 
-puts "Seu resultado é: #{number}"
+# pedir para inserir até 3 números
+while numeros.length < 3
+  puts "Insira um número:"
+  numero = gets.chomp.to_i
+  numeros << numero
+end
+
+# elevar cada número a 3ª potência e armazenar em um novo array
+resultados = []
+numeros.each do |numero|
+  resultado = numero ** 3
+  resultados << resultado
+end
+
+# imprimir o resultado
+puts "O resultado dos números elevados a 3ª potência é: #{resultados.join(', ')}"
---
+++ somaGPT.rb
@@ -0,0 +1,16 @@
+#entrada
+
+puts "Insira o primeiro numero: "
+numbe1 = gets.chomp.to_i
+puts "Insira o segundo numero: "
+numbe2 = gets.chomp.to_i
+puts "Insira o terceiro numero: "
+numbe3 = gets.chomp.to_i
+
+#calculo
+resultado1 = numbe1 ** 3
+resultado2 = numbe2 ** 3
+resultado3 = numbe3 ** 3
+
+#Resultado
+puts "Seu resultado das potencias é: #{resultado1}, #{resultado2}, #{resultado3}"
